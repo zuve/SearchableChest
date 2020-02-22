ea55cf5084589b5cc6bc7c8658fa963cb6a3e307
Zuve <zuvedev@gmail.com>
2020-02-22 16:24:27 -0700

    Deselect all text in search bar upon losing focus

--- src/main/java/zuve/searchablechests/ChestEventHandler.java
+++ src/main/java/zuve/searchablechests/ChestEventHandler.java
@@ -221,6 +221,8 @@ public class ChestEventHandler {
 			} else if (overSearchField) {
 				searchField.setCursorPositionEnd();
 				searchField.setSelectionPos(0);
+			} else {
+				searchField.setCursorPositionZero();
 			}
 		}
 	}
