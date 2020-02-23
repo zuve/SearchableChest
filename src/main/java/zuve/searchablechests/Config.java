86ae57443673c1eb7b479e2811da6cee98f99e1e
Zuve <zuvedev@gmail.com>
2020-02-22 22:45:37 -0700

    Formatting

--- src/main/java/zuve/searchablechests/Config.java
+++ src/main/java/zuve/searchablechests/Config.java
@@ -10,7 +10,10 @@ final class Config {
 	Config(final ForgeConfigSpec.Builder builder) {
 		autoFocus = builder.comment("Whether the search bar will be focused by default when opening containers")
 				.translation("searchablechests.config.autoFocus").define("autoFocus", false);
-		autoSelect = builder.comment("Whether the contents of the search bar will be selected when it is focused").translation("searchablechests.config.autoSelect").define("autoSelect", true);
-		minimumContainerSize = builder.comment("Minimum size a container must be for a search bar to be added").translation("searchablechests.config.minimumContainerSize").defineInRange("minimumContainerSize", 27, 0, Integer.MAX_VALUE);
+		autoSelect = builder.comment("Whether the contents of the search bar will be selected when it is focused")
+				.translation("searchablechests.config.autoSelect").define("autoSelect", true);
+		minimumContainerSize = builder.comment("Minimum size a container must be for a search bar to be added")
+				.translation("searchablechests.config.minimumContainerSize")
+				.defineInRange("minimumContainerSize", 27, 0, Integer.MAX_VALUE);
 	}
 }
