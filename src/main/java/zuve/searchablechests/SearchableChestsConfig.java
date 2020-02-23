6918501fea9e38cd5da8e640b16c5da1ca85f1f9
Zuve <zuvedev@gmail.com>
2020-02-22 22:50:42 -0700

    Add configuration option for highlighting text on focus

--- src/main/java/zuve/searchablechests/SearchableChestsConfig.java
+++ src/main/java/zuve/searchablechests/SearchableChestsConfig.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class SearchableChestsConfig {
 	public static boolean autoFocus;
+	public static boolean autoSelect;
 	public static ArrayList<String> blacklist;
 	public static int minimumContainerSize;
 }
